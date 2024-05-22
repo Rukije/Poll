b582b44fc488b57195642b6ba5a6718806ae052c
rukije <rukije.morina@harrisia.com>
2024-05-22 16:59:16 +0200

    Added button and modal to add question

--- src/app/polls/category.tsx
+++ src/app/polls/category.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Stack } from 'expo-router';
 import { Link } from 'expo-router';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -13,6 +13,10 @@ const polls = [
   { id: 6, title: 'Teknologji' },
   { id: 7, title: 'Shkence' },
   { id: 8, title: 'Muzike' },
+  { id: 5, title: 'Test' },
+  { id: 6, title: 'Test' },
+  { id: 7, title: 'Test' },
+  { id: 8, title: 'Test' },
 ];
 
 const getIconName = (title: string): string => {
@@ -33,13 +37,31 @@ const getIconName = (title: string): string => {
       return 'atom';
     case 'Muzike':
       return 'music';
+      case 'Sport':
+        return 'soccer';
+      case 'Teknologji':
+        return 'laptop';
+      case 'Shkence':
+        return 'atom';
+      case 'Muzike':
+        return 'music';
     default:
       return 'help-circle';
   }
 };
 
-
 export default function Category() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
+  const handleAddQuestion = () => {
+    //....
+    toggleModal();
+  };
+
   return (
     <>
       <Stack.Screen
@@ -67,7 +89,7 @@ export default function Category() {
               <View style={styles.pollContent}>
                 <Icon name={getIconName(item.title)} size={24} color="white" style={styles.icon} />
                 <Text style={styles.pollTitle}>
-                   {item.title}
+                  {item.title}
                 </Text>
               </View>
             </Link>
@@ -75,10 +97,19 @@ export default function Category() {
         )}
         keyExtractor={(item) => item.id.toString()}
       />
+      <TouchableOpacity onPress={toggleModal} style={styles.addButton}>
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Shto nje pyetje</Text>
+          <Button title="Shto" onPress={handleAddQuestion} />
+          <Button title="Close" onPress={toggleModal} />
+        </View>
+      </Modal>
     </>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -120,5 +151,36 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
     color: 'white',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'white',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 10, 
+    borderColor: '#193C47', 
+  },
+  addQuestion:{
+    backgroundColor: '#193C47',
+  },
+  addButtonText: {
+    color: '#193C47',
+    fontSize: 24,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 160,
+    color: '#193C47',
   },
 });
